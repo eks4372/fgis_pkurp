92a96a4b384bd80ad816e4c0ba6316d44b28e453
eks <nayrale@mail.ru>
2024-01-17 11:07:51 +0500

    мелкие доделки

--- t - e.py
+++ t - e.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def processing_file(filename="result\обращения.txt", file_out='корректировка прав.xlsx'):
+def processing_file(filename=r"result\обращения.txt", file_out='корректировка прав.xlsx'):
     '''Функция создает файл для бота "решение по правам"'''
     # Чтение файла
     with open(filename, 'r') as file:
@@ -26,4 +26,7 @@ def processing_file(filename="result\обращения.txt", file_out='корр
 
 t = input(f'введите название исходного файла (по умолчанию "result\обращения.txt"): ')
 # processing_file('result\обращения_17-01-2024 08_49.txt')
-processing_file(t)
+if t:
+    processing_file(t)
+else:
+    processing_file()
